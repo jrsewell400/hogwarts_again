1d21df071674dd2d06bf318eb90bef170aa2cddb
Jordan Sewell <jrsewell400@gmail.com>
2020-03-27 10:18:01 -0600

    Finish work on US4

--- spec/features/professors/show_spec.rb
+++ spec/features/professors/show_spec.rb
@@ -24,5 +24,26 @@ RSpec.describe "As a visitor,", type: :feature do
       expect(page).to have_content(longbottom.name)
       expect(page).to have_no_content(malfoy.name)
     end 
+
+    it "then I see the average age of all students for that professor." do
+      snape = Professor.create!(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create!(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create!(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create!(name: "Harry Potter" , age: 10 , house: "Gryffindor" )
+      malfoy = Student.create!(name: "Draco Malfoy" , age: 20 , house: "Slytherin" )
+      longbottom = Student.create!(name: "Neville Longbottom" , age: 30 , house: "Gryffindor" )
+
+      ProfessorStudent.create!(student: harry, professor: snape)
+      ProfessorStudent.create!(student: harry, professor: hagarid)
+      ProfessorStudent.create!(student: harry, professor: lupin)
+      ProfessorStudent.create!(student: malfoy, professor: hagarid)
+      ProfessorStudent.create!(student: malfoy, professor: lupin)
+      ProfessorStudent.create!(student: longbottom, professor: snape)
+
+      visit "/professors/#{snape.id}"
+      
+      expect(page).to have_content(20)
+    end
   end 
 end 
--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,27 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe "instance methods" do 
+    it "#average_age" do 
+      snape = Professor.create(name: "Severus Snape", age: 45, specialty: "Potions")
+      hagarid = Professor.create(name: "Rubus Hagarid", age: 38 , specialty: "Care of Magical Creatures")
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+
+      harry = Student.create!(name: "Harry Potter" , age: 10 , house: "Gryffindor" )
+      malfoy = Student.create!(name: "Draco Malfoy" , age: 20 , house: "Slytherin" )
+      longbottom = Student.create!(name: "Neville Longbottom" , age: 30 , house: "Gryffindor" )
+
+      ProfessorStudent.create!(student: harry, professor: snape)
+      ProfessorStudent.create!(student: harry, professor: hagarid)
+      ProfessorStudent.create!(student: harry, professor: lupin)
+      ProfessorStudent.create!(student: malfoy, professor: hagarid)
+      ProfessorStudent.create!(student: longbottom, professor: lupin)
+      ProfessorStudent.create!(student: longbottom, professor: snape)
+
+      expect(snape.average_age).to eq(20)
+      expect(hagarid.average_age).to eq(15)
+      expect(lupin.average_age).to eq(20)
+    end
+  end
 end
